7b6ae819ab3e7810e1bddc6c131ced20075b2e84
anders <atl@pck.dk>
2023-04-20 18:19:04 +0200

    Delete DeployWindowsLAPS.bat

--- GroupPolicy-WindowsLAPS/DeployWindowsLAPS.bat
+++
@@ -1,2 +0,0 @@
-CD /d %~dp0
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0GroupPolicy-WindowsLAPS.ps1""' -Verb RunAs}"
